978b66c02113fa05b52a9933a417e34398e5e584
matheson-alexis-8764 <alexis.matheson.8764@mail.linnbenton.edu>
2021-05-31 18:12:43 -0700

    Changed print statements to return statements

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,13 +3,13 @@
 
 def checknum(num):
     if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
+        return "FizzBuzz"
     elif num % 3 == 0:
-        print("Fizz")
+        return "Fizz"
     elif num % 5 == 0:
-        print("Buzz")
+        return "Buzz"
     else:
-        print(num)
+        return num
 
 
 def main():
